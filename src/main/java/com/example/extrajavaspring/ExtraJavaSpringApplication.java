11a065e9f03d1129edf4b2949ca7ccf1f3ea0dab
adilamirov <adil.amirov.02@gmial.com>
2026-04-27 14:09:25 +0300

    test commit

--- src/main/java/com/example/extrajavaspring/ExtraJavaSpringApplication.java
+++ src/main/java/com/example/extrajavaspring/ExtraJavaSpringApplication.java
@@ -8,6 +8,7 @@ public class ExtraJavaSpringApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ExtraJavaSpringApplication.class, args);
+        System.out.println("Hello World");
     }
 
 }
