596fadda8b3e0bd0d133ebdd01b953c59cc2a1b1
adilamirov <adil.amirov.02@gmial.com>
2026-04-27 14:06:04 +0300

    init commit

---
+++ src/main/java/com/example/extrajavaspring/ExtraJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.extrajavaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtraJavaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExtraJavaSpringApplication.class, args);
+    }
+
+}
